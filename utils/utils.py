eb30bbf11f99a19c95f4b366972ac95ac2fe4eb0
WwLuo-1024 <abrahamwong1408@gmail.com>
2022-03-07 20:25:15 +0000

    Utils Update

--- utils/utils.py
+++ utils/utils.py
@@ -124,12 +124,21 @@ class strLabelConverter(object):
             return texts
 
 def model_info(model):
-    pass
+    n_p = sum(x.numel() for x in model.parameters()) #number of parameters
+    n_g = sum(x.numel() for x in model.parameters() if x.requires_grad) #number gradients
+    print('\n%5s %50s %9s %12s %20s %12s %12s' % ('layer','name', 'gradient', 'shape', 'mu', 'sigma'))
+    for i, (name, p) in enumerate(model.named_parameters()):
+        name = name.replace('module_list.','')
+        print('%5s %50s %9s %12g %20g %12.3g %12.3g' % (
+            i, name, p.requires_grad, p.numel(), list(p.shape), p.mean(), p.std()
+        ))
+    print('Model Summary: %g layers, %g parameters, %g gradients\n' % (i + 1, n_p, n_g))
 
 if __name__ == '__main__':
+    pass
     # print(list(alphabets))
-    dict = {}
-    for i, char in enumerate(alphabets.alphabet):
-        dict[char] = i + 1
-        print(dict[char])
-        print(dict)
+    # dict = {}
+    # for i, char in enumerate(alphabets.alphabet):
+    #     dict[char] = i + 1
+    #     print(dict[char])
+    #     print(dict)
